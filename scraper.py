e48c2f82c2d94cf658ea5a24854316b35b650af4
Ben Bradshaw <ben.e.bradshaw@gmail.com>
2023-03-20 22:46:35 -0500

    s

--- scraper.py
+++ scraper.py
@@ -67,24 +67,16 @@ def scrape_site_for_email(url):
         
 def parse_facebook_for_email(url):
     chrome_options = Options()
-    user_agent = user_agent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.107 Safari/537.36"
     chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
     chrome_options.add_argument("--headless")
     chrome_options.add_argument("--disable-dev-shm-usage")
     chrome_options.add_argument("--no-sandbox")
-    chrome_options.add_argument(f'user-agent={user_agent}')
     chrome_options.add_argument('start-maximized')
-    print("HERE")
     driver = uc.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), options=chrome_options)
     driver.get(url)
-    print("HERE2")
     wait = WebDriverWait(driver, 5)
-    print("HERE2")
     wait.until(EC.presence_of_element_located((By.XPATH, "/html/body")))
-    PRINT("HERE#")
     plain_text = driver.find_element(By.XPATH, "/html/body").text
-    PRINT("HERE4")
-    print("SCRAPING THE BOOK")
     print(plain_text)
     no_newline = plain_text.strip('\n')
     email = re.findall(r"[a-z0-9\.\-+_]+@[a-z0-9\.\-+_]+\.[a-z]+", no_newline)
